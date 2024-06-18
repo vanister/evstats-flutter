421963f58c7194e780ed0211b8a47005f646f6cb
vansiter <vanister@users.noreply.github.com>
2024-06-17 22:19:10 -0700

    Fixed model test

--- test/models/session_test.dart
+++ test/models/session_test.dart
@@ -5,13 +5,14 @@ void main() {
   group('Session Model', () {
     test('should create a model', () {
       final now = DateTime.now();
-      final model = Session(vehicleId: 1);
+      final model = Session(vehicleId: 1, rateId: 1, kWh: 42, date: now);
 
       expect(model, isNotNull);
       expect(model.id, isNull);
       expect(model.vehicleId, equals(1));
-      expect(model.kWh, equals(0));
-      expect(model.date, equals(DateTime(now.year, now.month, now.day)));
+      expect(model.kWh, equals(42));
+      expect(model.date, equals(now));
+      expect(model.rateId, equals(1));
     });
   });
 }
