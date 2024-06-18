272306cb31bf78a7844d65ca615a7383dc970acc
vansiter <vanister@users.noreply.github.com>
2024-06-18 09:14:06 -0700

    Added remaining CRUD operations to DbContext

--- lib/data/db_context.dart
+++ lib/data/db_context.dart
@@ -7,6 +7,15 @@ abstract class DbContext {
   /// Runs a [sql] query against the database and returns a
   /// list of Map objects as a result.
   Future<List<Map>> query(String sql, [List<Object>? params]);
+
+  /// Runs a [sql] insert query against the databse.
+  Future<int> insert(String sql, [List<Object>? params]);
+
+  /// Runs a [sql] update query against the databse.
+  Future<int> update(String sql, [List<Object>? params]);
+
+  /// Runs a [sql] delete query against the databse.
+  Future<int> delete(String sql, [List<Object>? params]);
 }
 
 class SqliteDbContext implements DbContext {
@@ -24,6 +33,27 @@ class SqliteDbContext implements DbContext {
   @override
   Future<List<Map>> query(String sql, [List<Object>? params]) async {
     final results = await _database.rawQuery(sql, params);
+
+    return results;
+  }
+
+  @override
+  Future<int> insert(String sql, [List<Object>? params]) async {
+    final results = await _database.rawInsert(sql, params);
+
+    return results;
+  }
+
+  @override
+  Future<int> update(String sql, [List<Object>? params]) async {
+    final results = await _database.rawUpdate(sql, params);
+
+    return results;
+  }
+
+  @override
+  Future<int> delete(String sql, [List<Object>? params]) async {
+    final results = await _database.rawDelete(sql, params);
 
     return results;
   }
