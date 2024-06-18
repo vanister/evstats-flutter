8c471d090fe1d98e5f03bbe9a6adce90c6d8865c
vansiter <vanister@users.noreply.github.com>
2024-06-17 22:16:54 -0700

    Updated session model

--- lib/models/session.dart
+++ lib/models/session.dart
@@ -1,40 +1,30 @@
+import '../widgets/session/session_form.dart';
+
 class Session {
   final int? id;
   final int? vehicleId;
-
-  late DateTime date;
-  int kWh = 0;
-  int? rateId;
   double? rateOverride;
+
+  DateTime date = DateTime.now();
+  int kWh = 0;
+  int rateId;
 
   Session({
     this.id,
     this.vehicleId,
-    DateTime? date,
-    int kWh = 0,
-    int rateId = 1, // home
     this.rateOverride,
-  }) {
-    var now = DateTime.now();
+    required this.kWh,
+    required this.date,
+    required this.rateId,
+  });
 
-    date = DateTime(now.year, now.month, now.day);
-  }
-
-  bool get isValid => _validate();
-
-  bool _validate() {
-    if (kWh <= 0) {
-      return false;
-    }
-
-    if (rateId == null) {
-      return false;
-    }
-
-    if (rateOverride != null && rateOverride! <= 0) {
-      return false;
-    }
-
-    return true;
+  factory Session.fromFormEntry(SessionForm form, {int? id, int? vehicleId}) {
+    return Session(
+      id: id,
+      vehicleId: vehicleId,
+      date: form.date,
+      kWh: form.kWh,
+      rateId: form.rateType,
+    );
   }
 }
